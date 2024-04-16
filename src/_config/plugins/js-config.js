255eabb4452a9a0b747172ef6502aa0d633700a6
madrilene <hola@lenesaile.com>
2024-04-16 10:56:49 +0200

    deps

--- src/_config/plugins/js-config.js
+++ src/_config/plugins/js-config.js
@@ -1,20 +1,27 @@
 import esbuild from 'esbuild';
+import path from 'node:path';
 
 export const jsConfig = eleventyConfig => {
   eleventyConfig.addTemplateFormats('js');
 
   eleventyConfig.addExtension('js', {
     outputFileExtension: 'js',
-    compile: async (content, path) => {
-      if (!path.startsWith('./src/assets/scripts/')) {
+    compile: async (content, inputPath) => {
+
+			if (!inputPath.startsWith('./src/assets/scripts/')) {
         return;
       }
 
-      if (path === './src/assets/scripts/is-land.js') {
+      if (inputPath.startsWith('./src/assets/scripts/inline/')) {
+        const filename = path.basename(inputPath);
+        const outputFilename = filename.replace(/\.js$/, '-inline.js');
+        const outputPath = `./src/_includes/scripts/${outputFilename}`;
+
         await esbuild.build({
           target: 'es2020',
-          entryPoints: [path],
-          outfile: './src/_includes/is-land-inline.js',
+					platform: 'node',
+          entryPoints: [inputPath],
+          outfile: outputPath,
           bundle: true,
           minify: true
         });
@@ -24,7 +31,7 @@ export const jsConfig = eleventyConfig => {
       return async () => {
         let output = await esbuild.build({
           target: 'es2020',
-          entryPoints: [path],
+          entryPoints: [inputPath],
           minify: true,
           bundle: true,
           write: false
