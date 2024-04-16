84caeab209cfb23206d6122028a5361e3e043dde
madrilene <hola@lenesaile.com>
2024-04-16 12:37:48 +0200

    Default handling for other scripts, excluding inline scripts

--- src/_config/plugins/js-config.js
+++ src/_config/plugins/js-config.js
@@ -11,6 +11,7 @@ export const jsConfig = eleventyConfig => {
         return;
       }
 
+      // inline scripts go into the _includes folder
       if (inputPath.startsWith('./src/assets/scripts/inline/')) {
         const filename = path.basename(inputPath);
         const outputFilename = filename.replace(/\.js$/, '-inline.js');
@@ -26,17 +27,20 @@ export const jsConfig = eleventyConfig => {
         return;
       }
 
-      return async () => {
-        let output = await esbuild.build({
-          target: 'es2020',
-          entryPoints: [inputPath],
-          minify: true,
-          bundle: true,
-          write: false
-        });
+      // Default handling for other scripts, excluding inline scripts
+      if (!inputPath.startsWith('./src/assets/scripts/inline/')) {
+        return async () => {
+          let output = await esbuild.build({
+            target: 'es2020',
+            entryPoints: [inputPath],
+            minify: true,
+            bundle: true,
+            write: false
+          });
 
-        return output.outputFiles[0].text;
-      };
+          return output.outputFiles[0].text;
+        };
+      }
     }
   });
 };
